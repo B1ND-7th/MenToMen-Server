c2d47ad026a0a39bd831871fbbbebef682b88023
이지민 <wlals060330@gmail.com>
2022-08-22 20:22:11 +0900

    init

---
+++ src/main/java/project/bind/MenToMen/MenToMenApplication.java
@@ -0,0 +1,13 @@
+package project.bind.MenToMen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenToMenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenToMenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/bind/MenToMen/MenToMenApplicationTests.java
@@ -0,0 +1,13 @@
+package project.bind.MenToMen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenToMenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
