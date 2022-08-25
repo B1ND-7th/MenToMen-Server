dd8d18da840f9c70335cff79d8c6413d92b1860f
이지민 <wlals060330@gmail.com>
2022-08-25 17:14:42 +0900

    ADD: ErrorResponse

---
+++ src/main/java/project/bind/MenToMen/global/error/ErrorResponseEntity.java
@@ -0,0 +1,23 @@
+package project.bind.MenToMen.global.error;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@Builder
+public class ErrorResponseEntity {
+    private int status;
+    private String code;
+    private String message;
+
+    public static ResponseEntity<ErrorResponseEntity> responseEntity(ErrorCode e){
+        return ResponseEntity
+                .status(e.getHttpStatus())
+                .body(ErrorResponseEntity.builder()
+                        .status(e.getHttpStatus().value())
+                        .code(e.name())
+                        .message(e.getMessage())
+                        .build());
+    }
+}
