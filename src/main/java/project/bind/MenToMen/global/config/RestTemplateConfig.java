160202c7fbb667d011821b69d77eeb1320a0f43a
이지민 <wlals060330@gmail.com>
2022-08-25 17:15:09 +0900

    ADD: config

--- src/main/java/project/bind/MenToMen/global/config/RestTemplateConfig.java
+++ src/main/java/project/bind/MenToMen/global/config/RestTemplateConfig.java
@@ -1,4 +1,4 @@
-package project.bind.MenToMen.global.config.restTemplate;
+package project.bind.MenToMen.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
