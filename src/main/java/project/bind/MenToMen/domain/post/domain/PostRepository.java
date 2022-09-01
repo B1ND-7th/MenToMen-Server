32631cc19b5e3768a4584fefb31d51329df811a7
이지민 <wlals060330@gmail.com>
2022-09-01 21:05:33 +0900

    ADD: JPQL 쿼리문 추가

--- src/main/java/project/bind/MenToMen/domain/post/domain/PostRepository.java
+++ src/main/java/project/bind/MenToMen/domain/post/domain/PostRepository.java
@@ -1,4 +1,19 @@
 package project.bind.MenToMen.domain.post.domain;
 
-public interface PostRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import project.bind.MenToMen.domain.post.domain.entity.Post;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query("select p from Post p join fetch p.user u")
+    List<Post> findAll();
+
+    @Query("select p from Post p join fetch p.user u where p.Id=:id")
+    Optional<Post> findById(Long id);
 }
