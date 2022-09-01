1025fed32c7ea63d9422c4772b57b13b77702193
이지민 <wlals060330@gmail.com>
2022-09-01 21:02:53 +0900

    ADD: 전체, 단일 게시물 조회 기능 추가

--- src/main/java/project/bind/MenToMen/domain/post/controller/PostController.java
+++ src/main/java/project/bind/MenToMen/domain/post/controller/PostController.java
@@ -1,23 +1,42 @@
 package project.bind.MenToMen.domain.post.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import project.bind.MenToMen.domain.post.domain.dto.PostSubmitDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import project.bind.MenToMen.domain.post.dto.PostResponseDto;
+import project.bind.MenToMen.domain.post.dto.PostsResponseDto;
+import project.bind.MenToMen.domain.post.dto.PostSubmitDto;
+import project.bind.MenToMen.domain.post.service.PostService;
+import project.bind.MenToMen.global.response.DataResponse;
+import project.bind.MenToMen.global.response.Response;
 
-import javax.validation.Valid;
+import java.util.List;
 
+@Api(tags = "Post-Controller")
 @RestController
 @RequestMapping(value = "/post")
 @RequiredArgsConstructor
 public class PostController {
 
-    @PostMapping(value = "/submit")
-    public String submit(final @Valid @RequestBody PostSubmitDto postSubmitDto) {
-
-        return "";
+    private final PostService postService;
+    @ApiOperation(value = "게시물 등록")
+    @PostMapping("/submit")
+    public ResponseEntity<Response> post(@RequestBody PostSubmitDto postSubmitDto) {
+        postService.post(postSubmitDto);
+        return Response.ok("게시물 등록 성공");
     }
 
+    @ApiOperation(value = "전체 게시물 조회")
+    @GetMapping("/readAll")
+    public ResponseEntity<DataResponse<List<PostsResponseDto>>> readAll() {
+        return DataResponse.ok("전체 게시물 조회 성공", postService.findPostAll());
+    }
+
+    @ApiOperation(value = "단일 게시물 조회")
+    @GetMapping("/readOne/{postId}")
+    public ResponseEntity<DataResponse<PostResponseDto>> readOne(@PathVariable("postId") Long postId) {
+        return DataResponse.ok("단일 게시물 조회 성공", postService.findPostOne(postId));
+    }
 }
--- src/main/java/project/bind/MenToMen/domain/post/service/PostService.java
+++ src/main/java/project/bind/MenToMen/domain/post/service/PostService.java
@@ -1,16 +1,56 @@
 package project.bind.MenToMen.domain.post.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import project.bind.MenToMen.domain.post.domain.dto.PostSubmitDto;
-import project.bind.MenToMen.domain.post.domain.entities.Post;
+import project.bind.MenToMen.domain.post.domain.PostRepository;
+import project.bind.MenToMen.domain.post.dto.PostResponseDto;
+import project.bind.MenToMen.domain.post.dto.PostsResponseDto;
+import project.bind.MenToMen.domain.post.dto.PostSubmitDto;
+import project.bind.MenToMen.domain.post.domain.entity.Post;
+import project.bind.MenToMen.domain.user.domain.User;
+import project.bind.MenToMen.domain.user.domain.UserRepository;
+import project.bind.MenToMen.global.error.CustomError;
+import project.bind.MenToMen.global.error.ErrorCode;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
-@Transactional
+@RequiredArgsConstructor
+@Slf4j
 public class PostService {
 
-    public Post submit(PostSubmitDto postSubmitDto) {
-        return null;
+    @Value("${product.IPv4}")
+    private String IP;
+
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public Post post(PostSubmitDto postSubmitDto) {
+        User user = checkUser(postSubmitDto.getUserId());
+        return postRepository.save(postSubmitDto.toEntity(postSubmitDto, user));
     }
 
+    private User checkUser(Long userId) {
+        return userRepository.findById(userId).orElseThrow(() -> {
+            throw CustomError.of(ErrorCode.NOT_FOUND);
+        });
+    }
+
+    public List<PostsResponseDto> findPostAll() {
+        return postRepository.findAll().stream()
+                .map(post -> new PostsResponseDto(post))
+                .collect(Collectors.toList());
+    }
+
+    public PostResponseDto findPostOne(Long postId) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> {
+            throw CustomError.of(ErrorCode.NOT_FOUND);
+        });
+        return new PostResponseDto(post);
+    }
 }
