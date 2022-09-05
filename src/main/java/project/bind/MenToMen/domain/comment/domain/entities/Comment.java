f746921e97ae42661ed7a5e0a241554dbb39de49
rlawhddbs <savagestrikekr@gmail.com>
2022-09-05 18:00:11 +0900

    FIX: Entity 연관관계 설정

--- src/main/java/project/bind/MenToMen/domain/comment/domain/entities/Comment.java
+++ src/main/java/project/bind/MenToMen/domain/comment/domain/entities/Comment.java
@@ -2,6 +2,8 @@ package project.bind.MenToMen.domain.comment.domain.entities;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import project.bind.MenToMen.domain.post.domain.entities.Post;
+import project.bind.MenToMen.domain.user.domain.User;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -12,7 +14,15 @@ import java.time.LocalDateTime;
 public class Comment {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long commentId;
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
 
     @Column(nullable = false)
     private LocalDateTime dateTime;
