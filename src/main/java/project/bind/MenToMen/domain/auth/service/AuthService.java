a2053d5037149e287f986106f45f6bf960aec9ad
rlawhddbs <savagestrikekr@gmail.com>
2022-09-07 22:08:15 +0900

    FIX: Auth 관련 버그 수정

--- src/main/java/project/bind/MenToMen/domain/auth/service/AuthService.java
+++ src/main/java/project/bind/MenToMen/domain/auth/service/AuthService.java
@@ -1,6 +1,7 @@
 package project.bind.MenToMen.domain.auth.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -18,6 +19,7 @@ import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AuthService {
 
     private final RestTemplate restTemplate;
