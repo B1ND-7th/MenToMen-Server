9cfa8e1f1df1ac5c3a2362f4d331b262a79eb055
rlawhddbs <savagestrikekr@gmail.com>
2022-09-22 20:22:05 +0900

    ADD: 댓글 repository 추가

--- src/main/java/project/bind/MenToMen/domain/comment/domain/CommentRepository.java
+++ src/main/java/project/bind/MenToMen/domain/comment/domain/CommentRepository.java
@@ -1,4 +1,13 @@
 package project.bind.MenToMen.domain.comment.domain;
 
-public class CommentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import project.bind.MenToMen.domain.comment.domain.entities.Comment;
+import project.bind.MenToMen.domain.post.domain.entities.Post;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findAllByPost(Post post);
+
 }
