41bf8b7c583df39e56f8db001d83446e71d6c105
이지민 <wlals060330@gmail.com>
2022-10-04 23:47:37 +0900

    FIX: List File

--- src/main/java/project/bind/MenToMen/domain/file/service/FileService.java
+++ src/main/java/project/bind/MenToMen/domain/file/service/FileService.java
@@ -12,7 +12,10 @@ import project.bind.MenToMen.global.error.CustomError;
 import project.bind.MenToMen.global.error.ErrorCode;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,19 +26,27 @@ public class FileService {
 
     private final AmazonS3Client s3Client;
 
-    public ImgUrlResponseDto upload(MultipartFile file) throws IOException {
+    public List<ImgUrlResponseDto> upload(List<MultipartFile> files) throws IOException {
 
-        extensionCheck(file.getOriginalFilename());
+        List<ImgUrlResponseDto> imgUrlResponseDtoList = new ArrayList<>();
 
-        String s3FileName = UUID.randomUUID() + "-" + file.getOriginalFilename();
+        for (MultipartFile file : files) {
 
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(file.getSize());
-        metadata.setContentType(file.getContentType());
+            extensionCheck(file.getOriginalFilename());
 
-        s3Client.putObject(bucket, s3FileName, file.getInputStream(), metadata);
+            String ext = file.getOriginalFilename().split("\\.")[1];
 
-        return new ImgUrlResponseDto(s3Client.getUrl(bucket, s3FileName).toString());
+            String s3FileName = UUID.randomUUID() + "." + ext;
+
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(file.getSize());
+            metadata.setContentType(file.getContentType());
+
+            s3Client.putObject(bucket, s3FileName, file.getInputStream(), metadata);
+
+            imgUrlResponseDtoList.add(new ImgUrlResponseDto(s3Client.getUrl(bucket, s3FileName).toString()));
+        }
+        return imgUrlResponseDtoList;
     }
 
     private void extensionCheck(String imgUrl) {
@@ -52,8 +63,28 @@ public class FileService {
         if (check == false) throw CustomError.of(ErrorCode.WRONG_FILE);
     }
 
+    public void update(List<ImgUrlResponseDto> updateUrls, String originalUrls) {
+
+        List<String> updateFileNameList = updateUrls.stream().map(imgUrlResponseDto -> imgUrlResponseDto.getImgUrl().split("m/")[1]).collect(Collectors.toList());
+        List<String> originalFileNameList = List.of(originalUrls.split("///")).stream().map(url -> url.split("m/")[1]).collect(Collectors.toList());
+//        List<String> originalUrlList = List.of(originalUrls.split("///"));
+//        List<String> originalFileNameList = originalUrlList.stream().map(url -> url.split("m/")[1]).collect(Collectors.toList());
+
+        for (String originalFileName : originalFileNameList) {
+            boolean checkUrl = false;
+            for (String updateFileName : updateFileNameList) {
+                if(originalFileName.equals(updateFileName)) {
+                    checkUrl = true;
+                    break;
+                }
+            }
+            if (checkUrl == false) s3Client.deleteObject(new DeleteObjectRequest(bucket, originalFileName));
+        }
+    }
+
     public void delete(String imgUrl) {
         String fileName = imgUrl.split("m/")[1];
+        System.out.println(fileName);
         s3Client.deleteObject(new DeleteObjectRequest(bucket, fileName));
     }
 }
