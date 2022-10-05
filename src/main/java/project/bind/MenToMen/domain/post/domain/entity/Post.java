6499f98002269eb2c8daf0b3fec310365cf8cd78
rlawhddbs <savagestrikekr@gmail.com>
2022-10-05 16:52:49 +0900

    ADD: 영속성 전이 옵션 추가

--- src/main/java/project/bind/MenToMen/domain/post/domain/entity/Post.java
+++ src/main/java/project/bind/MenToMen/domain/post/domain/entity/Post.java
@@ -4,11 +4,14 @@ import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import project.bind.MenToMen.domain.comment.domain.entity.Comment;
 import project.bind.MenToMen.domain.post.domain.dto.PostUpdateDto;
 import project.bind.MenToMen.domain.user.domain.User;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Entity
@@ -25,6 +28,9 @@ public class Post {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "fk_user_id", nullable = false)
     private User user;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<Comment> commentList = new ArrayList<>();
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
